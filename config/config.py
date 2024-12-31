3b60953c9d5822483112744e85f5679a8a70af97
CreamMeatball <pch5445@gmail.com>
2024-12-31 10:08:36 +0900

    edit config

--- config/config.py
+++ config/config.py
@@ -6,8 +6,8 @@ load_dotenv()  # .env 파일에 정의된 환경변수 로드
 
 # OpenAI API 설정
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'YOUR_OPENAI_API_KEY')
-GPT_MODEL = "gpt-4o"
-MAX_TOKENS = 2000 # 최대 토큰. 높을 수록 긴 글 생성
+GPT_MODEL = "gpt-4o-mini"
+MAX_TOKENS = 4000 # 최대 토큰. 높을 수록 긴 글 생성
 TEMPERATURE = 0.7 # 높을 수록 더 다양한 결과 생성
 
 # WordPress 설정
